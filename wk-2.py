aefa2a5e9a6bb8af1efdcff0207afa86686ea0a8
hismyhill <owolabismaeel@gmail.com>
2025-09-02 13:12:53 +0100

    week-2 solution

---
+++ wk-2.py
@@ -0,0 +1,22 @@
+# Solution 1
+my_list= [];
+
+#  Solution 2
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Solution 3
+my_list.insert(1, 15)
+
+# Solution 4
+my_list.extend([50, 60, 70])
+
+#  Solution 5
+my_list.pop()
+
+# Solution 6
+print(my_list.index(30))
+
+# print(my_list)
